0c810ada583fe4a805b045e2ed820e033c3c481c
KMC1195 <kmckrzysiekw@gmail.com>
2023-01-11 09:42:55 +0100

    Adding tailwindcss, first component, basic styles

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+const SearchBar = () => {
+  return (
+    <div className="flex justify-center">
+      <input
+        type="text"
+        placeholder="Enter city here"
+        className="p-3 m-3 w-2/4 rounded-full outline-none focus:bg-gray-100 duration-150"
+      />
+      <button className="py-3 px-5 m-3 w-1/8 bg-white rounded-full hover:bg-gray-100 duration-150">
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
